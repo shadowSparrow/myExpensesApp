a1d1842f4650658ef21ba59c9feb2e1e84be1f63
shadowSparrow <romanenko.aleksandre@gmail.com>
2023-05-30 23:28:34 +0200

    Initial Commit

---
+++ myExpensesApp/ViewControllers/ExpensesViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ExpensesViewController.swift
+//  myExpensesApp
+//
+//  Created by mac on 30.05.2023.
+//
+
+import UIKit
+
+class ExpensesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.title = "Расходы"
+        //self.navigationController?.navigationBar.titleTextAttributes =
+        //= UIFont(name: "HelveticaNeueCyr", size: 28)
+        self.view.backgroundColor = UIColor.white
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ myExpensesApp/ViewControllers/TabBarViewController.swift
@@ -0,0 +1,42 @@
+//
+//  TabBarViewController.swift
+//  myExpensesApp
+//
+//  Created by mac on 23.05.2023.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    var expensesVC = UINavigationController(rootViewController: ExpensesViewController())
+    var expensesGraphVC = UINavigationController()
+    var incomeVC = UINavigationController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.viewControllers = [incomeVC,expensesGraphVC, expensesVC]
+        
+        //self.expensesVC.title = "Расходы"
+        self.expensesGraphVC.title = "График"
+        self.incomeVC.title = "Доходы"
+      
+        self.expensesVC.tabBarItem.image = UIImage(systemName: "circle")
+        self.expensesGraphVC.tabBarItem.image = UIImage(systemName: "circle")
+        self.incomeVC.tabBarItem.image = UIImage(systemName: "circle")
+        
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
