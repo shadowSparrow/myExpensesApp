a6ffa8217d563f2831e57773ece9ee6995de610f
shadowSparrow <romanenko.aleksandre@gmail.com>
2023-06-16 00:35:38 +0200

    detailTableView()

--- myExpensesApp/ViewControllers/ExpenseDetailsViewController.swift
+++ myExpensesApp/ViewControllers/ExpenseDetailsViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 class ExpenseDetailsViewController: UIViewController {
 
     var text: String = "Ремонт"
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.backgroundColor = .white
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -19,6 +28,17 @@ class ExpenseDetailsViewController: UIViewController {
         self.navigationController?.navigationBar.backgroundColor = .white
         self.navigationController?.navigationBar.tintColor = .black
         self.view.backgroundColor = UIColor.white
+        
+        self.view.addSubview(tableView)
+        let guide = self.view.safeAreaLayoutGuide
+        NSLayoutConstraint.activate([
+            tableView.leadingAnchor.constraint(equalTo: guide.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: guide.trailingAnchor),
+            tableView.topAnchor.constraint(equalTo: guide.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: guide.bottomAnchor, constant: -300)
+            ])
+        
+        
     }
     
 
@@ -32,4 +52,34 @@ class ExpenseDetailsViewController: UIViewController {
     }
     */
 
+}
+
+extension ExpenseDetailsViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .value1, reuseIdentifier: nil)
+        let image = UIImage(systemName: "chevron.right")
+        cell.backgroundColor = .white
+        cell.textLabel?.textColor = .black
+        
+        
+        let accessory = UIImageView(image: image)
+        accessory.tintColor = .blue
+        cell.accessoryView = accessory
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let detailVC = ExpenseDetailsViewController()
+        let cell = tableView.cellForRow(at: indexPath)
+        detailVC.text = cell?.textLabel?.text ?? "Wrong"
+        show(detailVC, sender: nil)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        64
+    }
 }
